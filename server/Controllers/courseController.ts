ddd0165f40f628086ae363fe3b38a41eace1c4dd
Muhammad Haris <imharis021@gmail.com>
2026-02-28 16:09:44 +0500

    Add edit course and get courses functionality with Redis caching

--- server/Controllers/courseController.ts
+++ server/Controllers/courseController.ts
@@ -4,6 +4,8 @@ import { catchAsyncError } from '../middlewares/catchAsyncErrors';
 import cloudinary from 'cloudinary';
 import { createCourse } from '../Services/courseService';
 import { AuthenticatedRequest } from '@/@types';
+import CourseModel from '../Models/courseModel';
+import { redis } from '../config/redis';
 
 // Upload Course
 export const uploadCourse = catchAsyncError(async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
@@ -25,3 +27,86 @@ export const uploadCourse = catchAsyncError(async (req: AuthenticatedRequest, re
         return next(new ErrorHandler(error.message, 500))
     }
 })
+
+// edit course 
+export const editCourse = catchAsyncError(async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    try {
+        const data = req.body;
+        const thumbnail = data.thumbnail;
+        if (thumbnail) {
+            await cloudinary.v2.uploader.destroy(thumbnail.public_id)
+            const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+                folder: "courses"
+            });
+
+            data.thumbnail = {
+                public_id: myCloud.public_id,
+                url: myCloud.secure_url,
+            }
+        }
+
+        const courseId = req.params.id;
+
+        const course = await CourseModel.findByIdAndUpdate(courseId, { $set: data }, { new: true })
+
+        res.status(201).json({
+            success: true,
+            course,
+        })
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 500))
+    }
+})
+
+// Get Single Course - not purchased
+export const getSingleCourse = catchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const course = await CourseModel
+            .findById(req.params.id)
+            // Choosing not send this data. This will be sent when user will but the course.
+            .select(
+                "-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links"
+            )
+
+        res.status(201).json({
+            success: true,
+            course
+        })
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 500))
+    }
+
+})
+
+// Get all Courses - not purchased 
+export const getAllCourses = catchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const isCacheExist = await redis.get("allCourses");
+
+        if (isCacheExist) {
+            console.log('hitting redis');
+            const courses = JSON.parse(isCacheExist);
+
+            return res.status(200).json({
+                success: true,
+                courses
+            });
+        }
+
+        console.log("hitting mongodb");
+        const courses = await CourseModel.find().select(
+            "-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links"
+        );
+
+
+        await redis.set("allCourses", JSON.stringify(courses));
+
+        res.status(200).json({
+            success: true,
+            courses
+        });
+
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 500));
+    }
+});
--- server/routes/courseRoute.ts
+++ server/routes/courseRoute.ts
@@ -1,9 +1,13 @@
 import express from 'express';
-import { uploadCourse } from '../Controllers/courseController';
+import { editCourse, getAllCourses, getSingleCourse, uploadCourse } from '../Controllers/courseController';
 import { isAuthenticated, isAuthorized } from '../middlewares/auth';
 
 const courseRouter = express.Router();
 
 courseRouter.post("/create-course", isAuthenticated, isAuthorized('admin'), uploadCourse)
+courseRouter.put("/edit-course/:id", isAuthenticated, isAuthorized('admin'), editCourse)
+
+courseRouter.get("/get-course/:id", getSingleCourse)
+courseRouter.get("/get-courses", getAllCourses)
 
 export default courseRouter
