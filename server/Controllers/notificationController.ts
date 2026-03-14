5cf32c1edb057849d34663b5ae3e0f520d628cec
Muhammad Haris <imharis021@gmail.com>
2026-03-14 14:51:57 +0500

    Add cron job for automatic deletion of old read notifications and update dependencies

--- server/Controllers/notificationController.ts
+++ server/Controllers/notificationController.ts
@@ -3,6 +3,7 @@ import { Request, Response, NextFunction } from "express";
 import { catchAsyncError } from '../middlewares/catchAsyncErrors';
 import ErrorHandler from "../Utils/ErrorHandler";
 import { AuthenticatedRequest } from "@/@types";
+const cron = require('node-cron');
 
 // Notification 
 export const getNotifications = catchAsyncError(async (req: AuthenticatedRequest, res: Response, next: Function) => {
@@ -40,3 +41,17 @@ export const updateNotification = catchAsyncError(async (req: AuthenticatedReque
         return next(new ErrorHandler(error.message, 500))
     }
 })
+
+// delete Notification
+cron.schedule('0 0 * * *', async () => {
+    // 1. Calculate the exact time 30 days ago
+    const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+
+    // 2. Delete all read notifications older than that time
+    await NotificationModel.deleteMany({
+        status: "read",
+        createdAt: { $lt: thirtyDaysAgo }
+    });
+
+    console.log('Cleaned up old read notifications.');
+});
